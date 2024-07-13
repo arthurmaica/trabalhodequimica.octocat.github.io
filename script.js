2910057cc68c7126b124879e3d24bef04bdccccf
arthurmaica <arthurmaicaa@gmail.com>
2024-07-13 14:14:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
-    // Adiciona interatividade aqui, se necessário.
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
 });
